c410a0fe90236c2b64f52dbb2e2f737b70972fdd
said hourdane <Said.Hourdane@merkle.com>
2024-11-19 16:29:12 +0100

    Edit actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 const nextConfig = {
   output: "export",
+  images: {
+    loader: "default",
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp3|wav)$/i,
